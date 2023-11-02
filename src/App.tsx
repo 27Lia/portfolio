fbb2e7cdea113f3a7012beb9cbf648ec7ac2bb65
27Lia <117743861+27Lia@users.noreply.github.com>
2023-11-03 05:37:02 +0900

    chore: 메인페이지삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Box, Plane, PerspectiveCamera } from '@react-three/drei';
+import './App.css';
+import { Vector3 } from 'three';  // 추가된 부분
 
-import MainPage from './pages/MainPage';
-
-
-
-const App: React.FC = () => {
+const Avatar: React.FC = () => {
   return (
-<>
-<MainPage/>
-    </>
+    <Box position={[0, 0.5, 0]} args={[1, 1, 1]}>
+      <meshStandardMaterial color={'blue'} />
+    </Box>
   );
 };
+
+function App() {
+  return (
+    <div className="App">
+      <Canvas>
+        <ambientLight />
+        <PerspectiveCamera position={[0, 5, 5]} makeDefault />
+        <gridHelper args={[10, 10]} />
+        <Avatar />
+      </Canvas>
+    </div>
+  );
+}
 
 export default App;
