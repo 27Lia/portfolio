eb335b0cbe22981d0e39c99ca13e82fa70553db0
27Lia <117743861+27Lia@users.noreply.github.com>
2023-11-03 09:06:44 +0900

    chore:앱 컴포넌트 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,14 @@
 import React from 'react';
-import { Canvas, useLoader } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 import './App.css';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
-import { Plane } from "@react-three/drei";
-import { OrbitControls } from "@react-three/drei";
 
-
-const Avatar: React.FC = () => {
-  const mtl = useLoader(MTLLoader, '/12221_Cat_v1_l3.mtl'); 
-  const obj = useLoader(OBJLoader, '/12221_Cat_v1_l3.obj', loader => { 
-    mtl.preload();
-    loader.setMaterials(mtl);
-  });
-
-  return <primitive object={obj} position={[0, 0, 0]} scale={[0.1, 0.1, 0.2]} />;
-};
-
-const Floor: React.FC = () => {
-  return (
-<mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
-<Plane args={[5, 5]} />
-      <meshBasicMaterial color={'#888888'} />
-    </mesh>
-  );
-};
 
 
 function App() {
   return (
     <div className='App'>
     <Canvas>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[2, 2, 2]} />
-      <Floor />
-      <Avatar />
-      <OrbitControls />
+
     </Canvas>
     </div>
   );
